6ba02ee3b6a0a61576e05c0e5aec1a7b28187954
James Garner <jamesjohngarner@gmail.com>
2015-06-25 11:39:35 +0100

    Added static files and created home page

--- pluginservice/urls.py
+++ pluginservice/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from .views import Homepage
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^', include('pluginservice.apps.polls.urls', namespace="polls")),
+    url(r'^$', Homepage.as_view(), name='homepage'),
+
+    url(r'^polls/', include('pluginservice.apps.polls.urls', namespace="polls")),
     url(r'^', include('pluginservice.apps.api.urls', namespace="api")),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
