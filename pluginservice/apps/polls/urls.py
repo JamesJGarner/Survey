91eaceaef8f887c3cd9e5b6161d0d158e06aa768
James Garner <jamesjohngarner@gmail.com>
2015-07-01 10:19:17 +0100

    Updates

--- pluginservice/apps/polls/urls.py
+++ pluginservice/apps/polls/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
-from .views import PollList, PollCreate
+from .views import PollList, PollCreate, PollCreateSuccess, PollDetail
 
 
 urlpatterns = patterns(
     '',
     url(r'^manage/$', PollList.as_view(), name='PollList'),
     url(r'^create/$', PollCreate.as_view(), name='PollCreate'),
+    url(r'^manage/(?P<pk>\d+)$', PollDetail.as_view(), name='PollDetail'),
+    url(r'^manage/(?P<pk>\d+)/help/$', PollCreateSuccess.as_view(), name='success'),
 )
--- pluginservice/apps/polls/views.py
+++ pluginservice/apps/polls/views.py
@@ -1,10 +1,24 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, TemplateView, DetailView
 from .models import Poll
+from django.core.urlresolvers import reverse
+
 
 
 # Not important right now
 class PollList(ListView):
     model = Poll
 
+class PollDetail(DetailView):
+    model = Poll
+
 class PollCreate(CreateView):
-	model = Poll
+    model = Poll
+    #success_url = '/polls/create/success/'
+    
+    def get_success_url(self, **kwargs):         
+        return reverse('polls:success', kwargs={'pk': self.object.id})
+
+class PollCreateSuccess(DetailView):
+    model = Poll
+    template_name = 'polls/poll_success.html'
+
