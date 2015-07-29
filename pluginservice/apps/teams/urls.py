04cb19468ae8a7d4ae6f6373e50c760cb6ee2a38
James Garner <jamesjohngarner@gmail.com>
2015-07-29 16:56:33 +0100

    Added ability to add teams

--- pluginservice/apps/teams/urls.py
+++ pluginservice/apps/teams/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
-from .views import TeamHome
+from .views import TeamHome, TeamCreate
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TeamHome.as_view(), name='Create'),
+    url(r'^$', TeamHome.as_view(), name='home'),
+    url(r'^create$', TeamCreate.as_view(), name='Create'),
+
+    
 )
--- pluginservice/apps/teams/views.py
+++ pluginservice/apps/teams/views.py
@@ -1,9 +1,23 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.models import User
-
+from .models import Team, TeamMate
+from django.core.urlresolvers import reverse
 
 
 class TeamHome(TemplateView):
     template_name = 'teams/team_home.html'
 
-    
+class TeamCreate(CreateView):
+    model = Team
+
+    def form_valid(self, form):
+        TeamForm = form.save(commit=False)
+        self.object = form.save()
+
+        AddUser = TeamMate.objects.create(team=self.object, user=self.request.user, admin=True)
+        AddUser.save()
+
+        return super(TeamCreate, self).form_valid(form)
+
+    def get_success_url(self, **kwargs):
+        return reverse('polls:PollList', kwargs={'pk': self.object.id})
