d7dc3bc99a253817f92f7a1d80dc743f3556ff4b
James Garner <jamesjohngarner@gmail.com>
2015-07-29 12:18:34 +0100

    Major design changes

--- pluginservice/apps/polls/urls.py
+++ pluginservice/apps/polls/urls.py
@@ -4,9 +4,9 @@ from .views import PollList, PollCreate, PollCreateSuccess, PollDetail, PollHome
 
 urlpatterns = patterns(
     '',
-    url(r'^$', PollHome.as_view(), name='PollHome'),
-    url(r'^manage/$', PollList.as_view(), name='PollList'),
+    #url(r'^$', PollHome.as_view(), name='PollHome'),
+    url(r'^$', PollList.as_view(), name='PollList'),
     url(r'^create/$', PollCreate.as_view(), name='PollCreate'),
-    url(r'^manage/(?P<pk>\d+)/$', PollDetail.as_view(), name='PollDetail'),
-    url(r'^manage/(?P<pk>\d+)/help/$', PollCreateSuccess.as_view(), name='success'),
+    url(r'^(?P<pk>\d+)/$', PollDetail.as_view(), name='PollDetail'),
+    url(r'^(?P<pk>\d+)/help/$', PollCreateSuccess.as_view(), name='success'),
 )
