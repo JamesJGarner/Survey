b4ba00b9c3a6854753c7824459f1323292cd8b31
James Garner <james@katodigital.com>
2015-07-29 19:34:39 +0100

    Updated styles

--- pluginservice/apps/teams/views.py
+++ pluginservice/apps/teams/views.py
@@ -1,11 +1,11 @@
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView
 from django.contrib.auth.models import User
 from .models import Team, TeamMate
 from django.core.urlresolvers import reverse
 
 
-class TeamHome(TemplateView):
-    template_name = 'teams/team_home.html'
+class TeamHome(ListView):
+    model = Team
 
 class TeamCreate(CreateView):
     model = Team
